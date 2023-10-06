580f7651444c83144fcf66afa758ee985324ccbb
M-Sanjay12o52o <msnjy12o52o@gmail.com>
2023-10-06 16:50:30 +0530

    another type error

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,24 +1,25 @@
-import { db } from "@/lib/db";
-import { PostVoteValidator } from "@/lib/validators/vote"
-import {getAuthSession} from "@/lib/auth"
-import type {CachedPost} from "@/types/redis"
-import {redis} from "@/lib/redis"
-import {z} from "zod";
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { redis } from '@/lib/redis'
+import { PostVoteValidator } from '@/lib/validators/vote'
+import type { CachedPost } from '@/types/redis'
+import { z } from 'zod'
 
-const CACHE_AFTER_UPVOTES = 1 
+const CACHE_AFTER_UPVOTES = 1
 
 export async function PATCH(req: Request) {
   try {
     const body = await req.json()
 
-    const {postId, voteType} = PostVoteValidator.parse(body)
+    const { postId, voteType } = PostVoteValidator.parse(body)
 
     const session = await getAuthSession()
 
-    if(!session?.user) {
-      return new Response('Unauthorized', {status: 401})
+    if (!session?.user) {
+      return new Response('Unauthorized', { status: 401 })
     }
 
+    // check if user has already voted on this post
     const existingVote = await db.vote.findFirst({
       where: {
         userId: session.user.id,
@@ -36,23 +37,46 @@ export async function PATCH(req: Request) {
       },
     })
 
-    if(!post) {
-      return new Response('Post not found', {status: 404})
+    if (!post) {
+      return new Response('Post not found', { status: 404 })
     }
 
-    if(existingVote) {
-      if(existingVote.type === voteType) {
+    if (existingVote) {
+      // if vote type is the same as existing vote, delete the vote
+      if (existingVote.type === voteType) {
         await db.vote.delete({
           where: {
             userId_postId: {
-              postId, 
+              postId,
               userId: session.user.id,
-            }
-          }
+            },
+          },
         })
+
+        // Recount the votes
+        const votesAmt = post.votes.reduce((acc, vote) => {
+          if (vote.type === 'UP') return acc + 1
+          if (vote.type === 'DOWN') return acc - 1
+          return acc
+        }, 0)
+
+        if (votesAmt >= CACHE_AFTER_UPVOTES) {
+          const cachePayload: CachedPost = {
+            authorUsername: post.author.username ?? '',
+            content: JSON.stringify(post.content),
+            id: post.id,
+            title: post.title,
+            currentVote: null,
+            createdAt: post.createdAt,
+          }
+
+          await redis.hset(`post:${postId}`, cachePayload) // Store the post data as a hash
+        }
+
         return new Response('OK')
       }
 
+      // if vote type is different, update the vote
       await db.vote.update({
         where: {
           userId_postId: {
@@ -65,10 +89,11 @@ export async function PATCH(req: Request) {
         },
       })
 
-      // recount the votes
+      // Recount the votes
       const votesAmt = post.votes.reduce((acc, vote) => {
-        if(vote.type === 'UP') return acc + 1
-        if(vote.type === 'DOWN') return acc - 1
+        if (vote.type === 'UP') return acc + 1
+        if (vote.type === 'DOWN') return acc - 1
+        return acc
       }, 0)
 
       if (votesAmt >= CACHE_AFTER_UPVOTES) {
@@ -81,12 +106,13 @@ export async function PATCH(req: Request) {
           createdAt: post.createdAt,
         }
 
-        await redis.hset(`post:${postId}`, cachePayload)
+        await redis.hset(`post:${postId}`, cachePayload) // Store the post data as a hash
       }
 
-      return new Response("OK")
+      return new Response('OK')
     }
 
+    // if no existing vote, create a new vote
     await db.vote.create({
       data: {
         type: voteType,
@@ -95,10 +121,11 @@ export async function PATCH(req: Request) {
       },
     })
 
-    // recount the votes
+    // Recount the votes
     const votesAmt = post.votes.reduce((acc, vote) => {
-      if(vote.type === 'UP') return acc + 1
-      if(vote.type === 'DOWN') return acc - 1
+      if (vote.type === 'UP') return acc + 1
+      if (vote.type === 'DOWN') return acc - 1
+      return acc
     }, 0)
 
     if (votesAmt >= CACHE_AFTER_UPVOTES) {
@@ -111,18 +138,19 @@ export async function PATCH(req: Request) {
         createdAt: post.createdAt,
       }
 
-      await redis.hset(`post:${postId}`, cachePayload)
+      await redis.hset(`post:${postId}`, cachePayload) // Store the post data as a hash
     }
 
     return new Response('OK')
-
   } catch (error) {
+    (error)
     if (error instanceof z.ZodError) {
-      return new Response("Invalid request data passed", { status: 422 });
+      return new Response(error.message, { status: 400 })
     }
 
-    return new Response("Could not your vote, please try again", {
-      status: 500,
-    });
+    return new Response(
+      'Could not post to subreddit at this time. Please try later',
+      { status: 500 }
+    )
   }
 }
